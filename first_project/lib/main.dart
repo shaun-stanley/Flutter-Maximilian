73b14ae667bf4e157be0cb3221b591f8143958a6
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2021-06-24 11:03:16 +0530

    add 'Scaffold'

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -5,9 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stanley'),
+        ),
+        body: Text('Hello World!'),
+      ),
     );
   }
 }
