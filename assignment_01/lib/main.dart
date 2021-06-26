ba3899d582f781b84fa8a755549b2df6d2b1a56d
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2021-06-26 16:35:52 +0530

    finish task1 and task2

--- assignment_01/lib/main.dart
+++ assignment_01/lib/main.dart
@@ -4,22 +4,35 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  String _printText = 'Hello World!';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Assignment 01'),
-        ),
-        body: Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
+          appBar: AppBar(
+            title: Text('Assignment 01'),
           ),
-        ),
-      ),
+          body: Column(children: [
+            Text(_printText),
+            // ignore: deprecated_member_use
+            RaisedButton(
+              child: Text('Change Text'),
+              onPressed: () {
+                setState(() {
+                  _printText = 'Hello Universe!';
+                });
+              },
+            ),
+          ])),
     );
   }
 }
