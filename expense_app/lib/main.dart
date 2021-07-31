b52f0a9515d2f62dccafa30ba5ef4d47534257a8
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2021-07-31 21:53:19 +0530

    reset app due to inactivity

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:ui';
-
-import './transactions.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,20 +15,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 64.99,
-      date: DateTime.now(),
-    ),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,58 +22,22 @@ class MyHomePage extends StatelessWidget {
         title: Text('iPay'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            width: double.maxFinite,
+            width: double.infinity,
             child: Card(
-                child: Container(
-                  color: Colors.amber,
-                  width: 100,
-                  child: Text(
-                    'Chart',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                elevation: 5),
+              color: Colors.amber,
+              elevation: 3,
+              child: Text('Hey'),
+            ),
           ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                elevation: 5,
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.amber, width: 2),
-                      ),
-                      padding: EdgeInsets.all(5),
-                      child: Text(
-                        tx.amount.toString(),
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(tx.date.toString()),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
+          Container(
+            child: Card(
+              color: Colors.amber,
+              elevation: 3,
+              child: Text('Bye'),
+            ),
+          )
         ],
       ),
     );
