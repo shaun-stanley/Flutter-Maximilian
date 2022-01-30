30c6bf0a0d4a9938c5ad98516fde7f82a4f29ec3
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2022-01-30 23:27:58 +0530

    Add list of cards to display transactions

--- New Repo/expense_app/lib/main.dart
+++ New Repo/expense_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_app/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List = [
+  final List transactions = [
     Transaction(
       id: 't1',
       title: "Apple Arcade",
@@ -43,8 +44,51 @@ class MyHomePage extends StatelessWidget {
                 child: Text("Graphs"),
               ),
             ),
-            Card(
-              child: Text("Information"),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  elevation: 5,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        child: Text(
+                          '\$' + tx.amount.toString(),
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue, width: 1),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('yMMMd').format(tx.date),
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.grey.shade800,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             )
           ],
         ));
