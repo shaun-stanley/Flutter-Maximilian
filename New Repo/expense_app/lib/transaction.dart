200c14fdfc1cc6a09a2d6a358f055588a4df8f0c
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2022-02-01 23:33:54 +0530

    Add comments

--- New Repo/expense_app/lib/transaction.dart
+++ New Repo/expense_app/lib/transaction.dart
@@ -4,5 +4,6 @@ class Transaction {
   final double amount;
   final DateTime date;
 
+  // Constructer that creates named parameters that can be used to assign values.
   Transaction({this.id, this.title, this.amount, this.date});
 }
